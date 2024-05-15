31a10170f79c4c446cd263a4780fa5fef55a7ad0
Audrow Nash <audrow@intrinsic.ai>
2024-05-15 17:30:30 -0500

    Imported upstream version '0.12.11' of 'upstream'

--- ament_clang_tidy/ament_clang_tidy/main.py
+++ ament_clang_tidy/ament_clang_tidy/main.py
@@ -154,8 +154,8 @@ def main(argv=sys.argv[1:]):
             cmd.append('--system-headers')
 
         def is_gtest_source(file_name):
-            if file_name == 'gtest_main.cc' or file_name == 'gtest-all.cc' \
-               or file_name == 'gmock_main.cc' or file_name == 'gmock-all.cc':
+            if(file_name == 'gtest_main.cc' or file_name == 'gtest-all.cc'
+               or file_name == 'gmock_main.cc' or file_name == 'gmock-all.cc'):
                 return True
             return False
 
@@ -165,14 +165,15 @@ def main(argv=sys.argv[1:]):
         def start_subprocess(full_cmd):
             output = ''
             try:
-                output = subprocess.check_output(
-                    full_cmd,
-                    stderr=subprocess.DEVNULL
-                ).decode()
+                output = subprocess.check_output(full_cmd,
+                                                 stderr=subprocess.DEVNULL).strip().decode()
             except subprocess.CalledProcessError as e:
                 print('The invocation of "%s" failed with error code %d: %s' %
                       (os.path.basename(clang_tidy_bin), e.returncode, e),
                       file=sys.stderr)
+                # Attempt to recover output, if any was found (eg - if
+                # WarningsAsErrors was specified in the config file).
+                output = e.output.decode('utf-8')
             return output
 
         files = []
@@ -203,7 +204,7 @@ def main(argv=sys.argv[1:]):
     for compilation_db in compilation_dbs:
         package_dir = os.path.dirname(compilation_db)
         package_name = os.path.basename(package_dir)
-        print(f"found compilation database for package '{package_name}' at '{compilation_db}'")
+        print('found compilation database for package "%s"...' % package_name)
         (source_files, output) = invoke_clang_tidy(compilation_db)
         files += source_files
         outputs.append(output)
@@ -277,13 +278,9 @@ def get_compilation_db_files(paths):
     for path in paths:
         if os.path.isdir(path):
             for dirpath, dirnames, filenames in os.walk(path):
-                # NOTE: here we don't check for the AMENT_IGNORE file.
-                # This function tries to find compile_commands.json file in the build folders.
-                # Build folders always include a AMENT_IGNORE file, so checking for it would
-                # result in not finding any compilation db. The check would also be redundant
-                # because if a source folder was marked with AMENT_IGNORE, then
-                # the compile_commands.json will not be present in relevant build folder.
-
+                if 'AMENT_IGNORE' in dirnames + filenames:
+                    dirnames[:] = []
+                    continue
                 # ignore folder starting with . or _
                 dirnames[:] = [d for d in dirnames if d[0] not in ['.', '_']]
                 dirnames.sort()
--- ament_xmllint/ament_xmllint/main.py
+++ ament_xmllint/ament_xmllint/main.py
@@ -29,7 +29,7 @@ from xml.sax.saxutils import quoteattr
 
 
 def main(argv=sys.argv[1:]):
-    default_extensions = ['xml']
+    extensions = ['xml']
 
     parser = argparse.ArgumentParser(
         description='Check XML markup using xmllint.',
@@ -38,10 +38,9 @@ def main(argv=sys.argv[1:]):
         'paths',
         nargs='*',
         default=[os.curdir],
-        help='The files or directories to check. For directories, only files ending '
-             'in %s will be considered (unless overruled by the --extensions '
-             'option)' %
-             ', '.join(["'.%s'" % e for e in default_extensions]))
+        help='The files or directories to check. For directories files ending '
+             'in %s will be considered.' %
+             ', '.join(["'.%s'" % e for e in extensions]))
     parser.add_argument(
         '--exclude',
         nargs='*',
@@ -50,11 +49,6 @@ def main(argv=sys.argv[1:]):
     # not using a file handle directly
     # in order to prevent leaving an empty file when something fails early
     parser.add_argument(
-        '--extensions',
-        nargs='*',
-        default=default_extensions,
-        help='The file extensions of the files to check')
-    parser.add_argument(
         '--xunit-file',
         help='Generate a xunit compliant XML file')
     args = parser.parse_args(argv)
@@ -62,7 +56,7 @@ def main(argv=sys.argv[1:]):
     if args.xunit_file:
         start_time = time.time()
 
-    files = get_files(args.paths, args.extensions, args.exclude)
+    files = get_files(args.paths, extensions, args.exclude)
     if not files:
         print('No files found', file=sys.stderr)
         return 1
