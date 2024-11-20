b405d845ce20ddfd7d8d146cbde7be3c58c79345
Chris Lalancette <clalancette@gmail.com>
2024-11-20 16:56:03 +0000

    Imported upstream version '0.19.0' of 'upstream'

--- ament_lint/setup.py
+++ ament_lint/setup.py
@@ -5,7 +5,7 @@ package_name = 'ament_lint'
 
 setup(
     name=package_name,
-    version='0.14.4',
+    version='0.19.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -32,6 +32,7 @@ setup(
 Providing common API for ament linter packages, e.g. the `linter` marker for
 pytest.""",
     license='Apache License, Version 2.0',
+    tests_require=['pytest'],
     entry_points={
         'pytest11': [
             'ament_lint = ament_lint.pytest_marker',
