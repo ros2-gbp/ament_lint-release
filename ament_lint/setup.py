511b8491abdcd14fb2059e29c5ef7cef9221d8a6
Chris Lalancette <clalancette@gmail.com>
2025-07-30 12:15:31 +0000

    Rebase from 'upstream'

--- ament_lint/setup.py
+++ ament_lint/setup.py
@@ -5,7 +5,7 @@ package_name = 'ament_lint'
 
 setup(
     name=package_name,
-    version='0.20.1',
+    version='0.20.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -23,7 +23,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -32,7 +31,11 @@ setup(
 Providing common API for ament linter packages, e.g. the `linter` marker for
 pytest.""",
     license='Apache License, Version 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'pytest11': [
             'ament_lint = ament_lint.pytest_marker',
