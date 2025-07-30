c235e04be06dbfdf278a55a6251448d332da5da0
Chris Lalancette <clalancette@gmail.com>
2025-07-30 12:15:47 +0000

    Rebase from 'upstream'

--- ament_uncrustify/setup.py
+++ ament_uncrustify/setup.py
@@ -5,7 +5,7 @@ package_name = 'ament_uncrustify'
 
 setup(
     name=package_name,
-    version='0.20.1',
+    version='0.20.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -27,7 +27,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -36,7 +35,11 @@ setup(
 The ability to check code against style conventions using uncrustify
 and generate xUnit test result files.""",
     license='Apache License, Version 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'ament_uncrustify = ament_uncrustify.main:main',
