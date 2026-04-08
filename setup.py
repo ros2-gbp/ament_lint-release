5a1814f534b3bf3b8ece2e001270c5aad467079a
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-04-08 12:54:08 +0200

    Rebase from 'release/rolling/ament_uncrustify'

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ package_name = 'ament_uncrustify'
 
 setup(
     name=package_name,
-    version='0.20.3',
+    version='0.20.4',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
     ],
-    install_requires=['setuptools'],
     package_data={'': [
         'configuration/ament_code_style_0_72.cfg',
         'configuration/ament_code_style_0_78.cfg',
